9f9786bf6d744ffe30a2bf0060a97fd23381e1bc
Dima <dmitry.korolev@gmail.com>
2022-09-10 22:17:38 +0400

    Actually using command line args in `examples/flags`.

--- examples/flags/flags.cc
+++ examples/flags/flags.cc
@@ -5,6 +5,7 @@
 DEFINE_int32(a, 2, "The `A` in `A+B`.");
 DEFINE_int32(b, 2, "The `B` in `A+B`.");
 
-int main() {
+int main(int argc, char** argv) {
+  ParseDFlags(&argc, &argv);
   std::cout << "A+B=" << FLAGS_a << '+' << FLAGS_b << '=' << FLAGS_a + FLAGS_b << std::endl;
 }
