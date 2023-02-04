ed5a6ea6fc8a85ba694e4045aeff8144db4bd3bc
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2023-02-03 22:35:36 -0800

    Added the ability to use custom container.

--- grpcpp.sh
+++ grpcpp.sh
@@ -2,7 +2,17 @@
 #
 # Usage from Linux: `alias grpcpp=/path/to/grpcpp.sh`.
 
-SRC=$1
+DEFAULT_DOCKER_CONTAINER=crnt/grpcpp
+DOCKER_CONTAINER=${GRPCPP_CONTAINER:-$DEFAULT_DOCKER_CONTAINER}
+
+if [ "$DOCKER_CONTAINER" != "$DEFAULT_DOCKER_CONTAINER" ] ; then
+  echo -e '\033[1m\033[36m=== CUSTOM CONTAINER ===\033[0m'
+  echo
+  echo 'Using GRPCPP_CONTAINER='$DOCKER_CONTAINER
+  echo
+fi
+
+SRC="$1"
 if [ ! -d "$SRC" ] ; then
   echo 'The first command line argument should be the name of the directory, under $PWD, to run `grpcpp` on.'
   exit 1
@@ -10,4 +20,5 @@ fi
 BUILD="$PWD/.build_$SRC"
 mkdir -p "$BUILD"
 shift
-docker run --network host -u $(id -u):$(id -g) -v "$PWD/$SRC":/src -v "$BUILD":/build -it crnt/grpcpp $*
+
+docker run --network host -u $(id -u):$(id -g) -v "$PWD/$SRC":/src -v "$BUILD":/build -it $DOCKER_CONTAINER $*
